3e641ab3bb44bb9067fd601164b4213318572fdd
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-13 21:46:20 -0300

    eliminé algunos códigos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 rotulo.innerHTML = "Indique la altura de la persona:";
-let alturaPermitida: number = 1.3;
-let alturaPersona: number = Number(dato1.value);
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
